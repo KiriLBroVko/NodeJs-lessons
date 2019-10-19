aee8a5dd6da00d8789439f50899600e54c7ec86e
KiriLBroVko <darthhotabvader@yahoo.com.ua>
2019-10-19 19:56:38 +0300

    Give object if it's id=3

--- tasks/funcArrayOfObj.js
+++ tasks/funcArrayOfObj.js
@@ -30,48 +30,16 @@ var arrayListOfObjects = [
         position: "AVP"
     }
 ];
-//console.log(arrayListOfObjects[2]);
-//console.log(Object.keys(arrayListOfObjects));
-//const arrayValue = Object.values(arrayListOfObjects); 
-//console.log(arrayListOfObjects);
-// const userPrivileges = ['user', 'user', 'user', 'admin'];
-// const containsAdmin = arrayListOfObjects.some( element => element === 'admin');
-// в containsAdmin будет записано true
-    arrayListOfObjects.forEach(function(item, index, array) {
-        //console.log(typeof(item));
-        // var arr = Object.values(item);
-        // console.log(arr);
-        //console.log(item);
-        var arrayKey = Object.keys(item);
-        console.log(arrayKey);
-        const containKey = arrayKey.some((element) => {
-            if (element === 'id' && true){
-                if () {}
-        }});
-        console.log(containKey);
-        //console.log(item[index]);
-        // arrayKey.forEach(function(item, index, array) {
-        //     if (item === 'id'){
-        //         // console.log("true");
-        //         // console.log(item[index]);
-        //     }
-        // });
-        
-    });
 
-// //function getThirdObj(array) {
-//     arrayListOfObjects.forEach(function(item, index, array) {
-//         // ... делать что-то с item
-//         const arrayKey = Object.keys(item);
-//         // console.log(arrayKey[0].values(item)); // no way !!!
-//         const arrayValue = Object.values(item); 
-//         //console.log(arrayValue[2]);
-//         if (arrayKey === 'id') {
-//             console.log("True");
-//             if (arrayValue === 3) {
-//                 console.log(array[2]);
-//             }
-//         }
-//     });
-// //}
-//getThirdObj(arrayListOfObjects);
+function getThirdObj(array) {
+    array.forEach(function(item, index, array) {
+        Object.keys(item)
+            .forEach(function eachKey(key) { 
+                if (key === 'id' && item[key]===3){
+                    console.log("true");
+                    console.log(item);
+                } 
+            });
+    });
+}
+getThirdObj(arrayListOfObjects);
