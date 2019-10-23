fa1e5f561818c3d3356e000f2dc532d53c9632c1
KiriLBroVko <darthhotabvader@yahoo.com.ua>
2019-10-23 23:01:02 +0300

    Array Includes task

--- tasks/arrayIncludes.js
+++ tasks/arrayIncludes.js
@@ -2,8 +2,8 @@
 // create an array and check if this array includes 5
 var arrayNumber = [1, 2, 5, 8, 10];
 
-function ifItsFiveinArray(array) {
-        return "Array includes " + array.includes(5);
+function ifItsFiveinArray(array) {    
+        return array.includes(5);
 };
 
 var result = ifItsFiveinArray(arrayNumber);
