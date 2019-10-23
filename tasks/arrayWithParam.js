9d2d3a2382fb64ce163644d776581f1577eed742
KiriLBroVko <darthhotabvader@yahoo.com.ua>
2019-10-23 23:10:58 +0300

    Array Includes task

--- tasks/arrayWithParam.js
+++ tasks/arrayWithParam.js
@@ -1,8 +1,8 @@
 var array = [1,2,4,5,6,7,8];
 
-function ifItFiveInArr(array, number){
+function ifNumberInArr(array, number){
     return array.includes(number);
 };
 
-var result = ifItFiveInArr(array, 5);
+var result = ifNumberInArr(array, 5);
 console.log(result);
